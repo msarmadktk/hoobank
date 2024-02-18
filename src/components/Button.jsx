0a94a1d0458e846dbb782e9672121d0d291deb8c
msarmadktk <114760949+msarmadktk@users.noreply.github.com>
2024-02-18 10:54:09 +0500

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,12 +9,3 @@ const Button = ({styles}) => {
 }
 
 export default Button
-// import React from "react";
-
-// const Button = ({ styles }) => (
-//   <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
-//     Get Started
-//   </button>
-// );
-
-// export default Button;
