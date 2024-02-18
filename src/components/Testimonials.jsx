824d90f4451e4519b7f09620136930c8ae642b39
msarmadktk <114760949+msarmadktk@users.noreply.github.com>
2024-02-18 10:55:10 +0500

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -22,21 +22,3 @@ const Testimonials = () => (
 );
 
 export default Testimonials;
-
-// <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
-
-// <div className="w-full flex justify-between items-center md:flex-row flex-col md:mb-16 mb-6 relative z-[1]">
-//   <h2 className={styles.heading2}>
-//     What People are <br className="md:block hidden" /> saying about us
-//   </h2>
-//   <div className="w-full md:mt-0 mt-6">
-//     <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-//       Everything you need to accept card payments and grow your business
-//       anywhere on the planet.
-//     </p>
-//   </div>
-// </div>
-
-// <div className="flex flex-wrap md:justify-start justify-center w-full feedback-container relative z-[1]">
-//   {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)}
-// </div>
